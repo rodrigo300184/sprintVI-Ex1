fc1968f6527626e26548df2ee36920b0b33aa346
Rodrigo <rodrigo300184@gmail.com>
2023-10-23 12:54:00 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+const http = require('node:http');
+
+
+const server = http.createServer((req,res) => {
+    console.log('Server created')
+    res.end('Hola, soy Skynet...todavia en desarrollo')
+})
+
+server.listen(3000,()=> {
+    console.log('El server esta perreando')
+})
