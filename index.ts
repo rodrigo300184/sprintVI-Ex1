5e88d00a4253fccc0dcb7c0ea0ffa9b13d8dfe38
Rodrigo <rodrigo300184@gmail.com>
2023-10-23 17:38:34 +0200

    CSV corregido

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import fs from 'node:fs';
+import fs, { writeFileSync } from 'node:fs';
 
 const data = fs.readFileSync('./countries.txt', 'utf-8').toString();
 const dataArray = data.split('\n');
-interface CountryData { countryName: string, population: string, area: string, density: string }
+interface CountryData { countryName: string, population: string, area: string, density: number }
 
 
 let resultArray: CountryData[] = []
@@ -16,13 +16,23 @@ dataArray.forEach(element => {
     const countryName = words.join(" ");
 
     if (!isNaN(parseInt(area)) && !isNaN(parseInt(population)) && isNaN(parseInt(countryName))) {
-        const country = { countryName: countryName, population: population, area: area, density: (parseInt(population) / parseInt(area)).toFixed(1) };
+        const country = { countryName: countryName, population: population, area: area, density: Math.round(parseInt(population) / parseInt(area)) };
 
         resultArray.push(country);
     }
 
 });
-resultArray.sort((a, b) => a.countryName.localeCompare(b.countryName))
+resultArray.sort((a, b) => b.density - a.density)
 
-console.log(resultArray)
+try {
+    const headers = 'Country,Population,Area,Density\n';
+
+    const csvData = resultArray.map(obj => `${obj.countryName},${obj.population},${obj.area},${obj.density}`).join('\n');
+
+    const csvContent = `${headers}${csvData}`;
+    fs.writeFileSync('archivo.csv',csvContent );
+    console.log('El archivo se escribió exitosamente.');
+} catch (error) {
+    console.error('Error al escribir el archivo:', error);
+}
 
